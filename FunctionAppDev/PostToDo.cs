5757c298ef45edeb00d381f2432d222e9c0de8f0
Fábio-Sudo <fabiotrevezane@gmail.com>
2024-10-30 08:15:35 -0300

    Adicionar arquivos de projeto.

---
+++ FunctionAppDev/PostToDo.cs
@@ -0,0 +1,52 @@
+using FunctionAppDev.Model;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.Functions.Worker.Extensions.Sql;
+
+namespace FunctionAppDev
+{
+    public static class PostToDo
+    {
+        // create a new ToDoItem from body object
+        // uses output binding to insert new item into ToDo table
+        [Function(nameof(PostToDo))]
+        public static async Task<OutputType> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "PostFunction")] HttpRequestData req,
+            FunctionContext executionContext)
+        {
+            var logger = executionContext.GetLogger("PostToDo");
+            logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            ToDoItem toDoItem = JsonConvert.DeserializeObject<ToDoItem>(requestBody);
+
+            // generate a new id for the todo item
+            toDoItem.Id = Guid.NewGuid();
+
+            // set Url from env variable ToDoUri
+            toDoItem.url = "google.com/" + "?id=" + toDoItem.Id.ToString();
+
+            // if completed is not provided, default to false
+            if (toDoItem.completed == null)
+            {
+                toDoItem.completed = false;
+            }
+
+            return new OutputType()
+            {
+                ToDoItem = toDoItem,
+                HttpResponse = req.CreateResponse(System.Net.HttpStatusCode.Created)
+            };
+        }
+    }
+
+    public class OutputType
+    {
+        [SqlOutput("dbo.ToDo", connectionStringSetting: "SqlConnectionString")]
+        public ToDoItem ToDoItem { get; set; }
+
+        public HttpResponseData HttpResponse { get; set; }
+    }
+}
